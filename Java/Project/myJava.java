98447ae176b249e33b939114481346861f5b3b61
Heeflower5001 <gmltmd3tp@naver.com>
2025-01-09 15:29:50 +0900

    25_01_09

--- Java/Project/myJava.java
+++ Java/Project/myJava.java
@@ -1,9 +1,11 @@
-
-
 // 패키지 오류 무시
 
 public class myJava {
     public static void main(String[] args) {
-        System.out.println("Hello, Java"); //df
+        byte byteValue = 10;
+        float floatValue = 2.5f;
+        double doubleValue = 2.5;
+
+        byte result = byteValue + byteValue;
     }
 }
