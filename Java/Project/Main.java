f754e54f33c63961d485576b53b61a583742f9ae
HeeFlower5001 <gmltmd3tp@naver.com>
2025-05-05 10:38:51 +0900

    2025_05_05

--- Java/Project/Main.java
+++ Java/Project/Main.java
@@ -4,18 +4,42 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // 직사각형1 입력 (a, b) ~ (c, d)
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
-        int d = sc.nextInt();
+        int n = sc.nextInt();
+        int m = sc.nextInt();
 
-        // 직사각형2 입력 (e, f) ~ (g, h)
-        int e = sc.nextInt();
-        int f = sc.nextInt();
-        int g = sc.nextInt();
-        int h = sc.nextInt();
+        int[][] coin = new int[n][m];
+        int[][] dp = new int[n][m];
 
-        int value1 = Math.
+        // coin 입력 받기
+        for (int i1 = 0; i1 < n; i1++) {
+            for (int i2 = 0; i2 < m; i2++) {
+                coin[i1][i2] = sc.nextInt();
+            }
+        }
+
+        // 0행, 0열 채우기
+        dp[0][0] = coin[0][0];
+
+        for (int i1 = 1; i1 < n; i1++) {
+            dp[i1][0] = dp[i1 - 1][0] + coin[i1][0];
+        }
+
+
+        for (int i2 = 1; i2 < m; i2++) {
+            dp[0][i2] = dp[0][i2 - 1] + coin[0][i2];
+        }
+
+        // 1행 ~ (n - 1)행 채우기
+        for (int i1 = 1; i1 < n; i1++) {
+            for (int i2 = 1; i2 < m; i2++) {
+                // 3가지 비교
+                int value1 = Math.min(dp[i1 - 1][i2], dp[i1][i2 - 1]);
+                int value2 = Math.min (value1, dp[i1 - 1][i2 - 1]);
+
+                dp[i1][i2] = value2 + coin[i1][i2];
+            }
+        }
+
+        System.out.print(dp[n - 1][m - 1]);
     }
 }
